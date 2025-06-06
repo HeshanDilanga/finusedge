d54b9e02fd6a8139282c20d42ca57b44755c4608
Pasindu-Adhikari <pasindumax39@gmail.com>
2025-06-06 19:49:06 +0530

    feat: Add Contact component for form submission and integrate into ContactSection

---
+++ src/components/footer2.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function Contact() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "9ed5955f-d2ec-4bb4-988a-9ce869d25bcd");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name" required/>
+        <input type="email" name="email" required/>
+        <textarea name="message" required></textarea>
+
+        <button type="submit">Submit Form</button>
+
+      </form>
+      <span>{result}</span>
+
+    </div>
+  );
+}
